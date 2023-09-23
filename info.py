6dfd58be2bf6617a2e0f79ade873274f2bde23d1
Prashantbaba000 <113820013+Prashantbaba000@users.noreply.github.com>
2023-09-23 14:56:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,8 +96,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://app.koyeb.com/apps/888c87dd-9db9-45c3-9e1b-a9af1df83cbf/services/81e2fafd-6ab8-49d0-9f3e-a351a5a9ec57".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://app.koyeb.com/apps/888c87dd-9db9-45c3-9e1b-a9af1df83cbf/services/81e2fafd-6ab8-49d0-9f3e-a351a5a9ec57/".format(FQDN, PORT)
+URL = "https://loyalprofessorrobot-prashantbaba000.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://loyalprofessorrobot-prashantbaba000.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://app.koyeb.com/apps/888c87dd-9db9-45c3-9e1b-a9af1df83cbf/services/81e2fafd-6ab8-49d0-9f3e-a351a5a9ec57/".format(FQDN)
+    URL = "https://loyalprofessorrobot-prashantbaba000.koyeb.app/".format(FQDN)
 else:
-    URL = "https://app.koyeb.com/apps/888c87dd-9db9-45c3-9e1b-a9af1df83cbf/services/81e2fafd-6ab8-49d0-9f3e-a351a5a9ec57/".format(FQDN)
+    URL = "https://loyalprofessorrobot-prashantbaba000.koyeb.app/".format(FQDN)
 
 
 
